a81c8eaeb52ee7433039736f5306a113c8a970c1
Indranil karmakar <karmakarindranil02@gmail.com>
2026-08-27 21:48:09 +0530

    Time: 10 ms (44.47%), Space: 11.3 MB (85.89%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -11,23 +11,22 @@ public:
     ListNode *detectCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
-
         while(fast != NULL && fast->next != NULL) {
             slow = slow->next;
             fast = fast->next->next;
-
             if(slow == fast) {
                 slow = head;
-
                 while(slow != fast) {
                     slow = slow->next;
                     fast = fast->next;
                 }
-
-                return slow;   // start of cycle
+                return slow;
             }
+            
+            
         }
+        return NULL;
+        
 
-        return NULL;   // no cycle
     }
 };
